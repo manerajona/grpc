31096a04122451a50cb9119aca5e120f1114ba96
manerjo <jonathan.manera@tecnotree.com>
2020-09-28 08:37:03 -0300

    client

--- 03-client/main.go
+++ 03-client/main.go
@@ -4,26 +4,25 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/manerajona/grpc/03-client/echo"
-	"google.golang.org/grpc"
+	echo "github.com/manerajona/grpc/03-client/echo"
+	grpc "google.golang.org/grpc"
 )
 
 func main() {
 	ctx := context.Background()
 
-	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
-	if err != nil {
-		panic(err)
-	}
+	conn, _ := grpc.Dial("localhost:8080", grpc.WithInsecure()) // no ssl
+
 	defer conn.Close()
+	{
+		e := echo.NewEchoServerClient(conn)
+		resp, err := e.Echo(ctx, &echo.EchoRequest{
+			Message: "Hello World!",
+		})
+		if err != nil {
+			panic(err)
+		}
 
-	e := echo.NewEchoServerClient(conn)
-	resp, err := e.Echo(ctx, &echo.EchoRequest{
-		Message: "Hello World!",
-	})
-	if err != nil {
-		panic(err)
+		fmt.Println("Got from server:", resp.Response)
 	}
-
-	fmt.Println("Got from server:", resp.Response)
 }
